70c15097e2350476b5856ce1b98491c1749f9d64
Elijah <elijahsharper1@gmail.com>
2018-02-05 22:25:56 +0300

    front end setup

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.default')
+
+@section('content')
+    <div class="row">
+        <div class="large-6 columns">
+            <div class="container">
+                test
+            </div>
+        </div>
+    </div>
+@endsection
